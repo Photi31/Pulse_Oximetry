9e559e9d8c0f778c3464786f11957ba2d24a4c66
svetlana nemova <photiniya@gmail.com>
2022-11-17 15:00:17 +0300

    Completed adaptive layout. Modal windows are done. Added scripts for modal windows and product cards.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,51 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const {src, dest, watch, parallel} = require('gulp');
 
-gulp.task('server', function() {
+const browserSync    = require('browser-sync').create();
+const sass           = require('gulp-sass')(require('sass'));
+const cleanCSS       = require('gulp-clean-css');
+const autoprefixer   = require('gulp-autoprefixer');
+const rename         = require("gulp-rename");
+const concat         = require('gulp-concat');
+const uglify         = require('gulp-uglify-es').default;
 
-    browserSync({
+function browsersync() {
+    browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "src/"
         }
     });
+}
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+function scripts() {
+    return src([
+        
+        'src/js/script.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('src/js'))
+    .pipe(browserSync.stream());     
+}
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+function styles() {
+    return src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+        .pipe(dest("src/css"))
+        .pipe(browserSync.stream());     
+}
 
+function watching() {
+    watch(['src/sass/**/*.+(scss|sass)'], styles);
+    watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
+    watch("src/*.html").on('change', browserSync.reload);
+}
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-});
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
 
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+exports.default = parallel(scripts, browsersync, watching);
--- src/js/script.js
+++ src/js/script.js
@@ -53,27 +53,36 @@ document.querySelector('.catalog__tab').click();
 
 
 const catalogItemContent = document.querySelectorAll('.catalog-item__content');
+const catalogItemLink = document.querySelectorAll('.catalog-item__link');
+const catalogItemBack = document.querySelectorAll('.catalog-item__back');
 
 catalogItemContent.forEach(function(elem) {
     elem.classList.add('catalog-item__content_active');
+});
+
+catalogItemLink.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        let parentBlock = elem.closest('.catalog-item__wrapper');
+        let itemContent = parentBlock.childNodes[1];
+        let itemList = parentBlock.childNodes[3];
+        let itemBack = parentBlock.childNodes[5];
+
+        activeItemList(itemContent, itemList, itemBack);
+    })
+})
+ 
+catalogItemBack.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        let parentBlock = elem.closest('.catalog-item__wrapper');
+        let itemContent = parentBlock.childNodes[1];
+        let itemList = parentBlock.childNodes[3];
+        let itemBack = parentBlock.childNodes[5];
+
+        activeItemContent(itemContent, itemList, itemBack);
+    })
 })
 
-
-
-//const catalogItem = document.querySelectorAll('.catalog-item');
-
-// catalogItem.forEach(function(elem) {
-//     let itemContent = elem.querySelector('.catalog-item__content_active');
-//     let itemLink = elem.querySelector('.catalog-item__link');
-//     let itemList = elem.querySelector('.catalog-item__list');
-//     let itemBack = elem.querySelector('.catalog-item__back');
-
-//     // itemLink.addEventListener('click', activeItemList(itemContent, itemList, itemBack));
-//     // itemBack.addEventListener('click', activeItemContent(itemContent, itemList, itemBack));
-// })
-
-function activeItemList(catalogItem) {
-    
+function activeItemList(itemContent, itemList, itemBack) {
     itemContent.classList.remove('catalog-item__content_active');
     itemList.classList.add('catalog-item__list_active');
     itemBack.classList.add('catalog-item__back_active');
@@ -83,4 +92,52 @@ function activeItemContent(itemContent, itemList, itemBack) {
     itemContent.classList.add('catalog-item__content_active');
     itemList.classList.remove('catalog-item__list_active');
     itemBack.classList.remove('catalog-item__back_active');
+}
+
+//modal
+
+const modal = document.querySelector('.modal');
+const modalClose = document.querySelectorAll('.modal__close');
+const buttonSubmit = document.querySelectorAll('.button_submit');
+
+// buttonSubmit.forEach(function(elem) {
+//     elem.addEventListener('click', function() {
+//         let close = elem.closest('.modal__wrapper').childNodes[1];
+//         activeModalClose(close);
+//         activeModalFhanks();
+//     })
+// })
+
+modalClose.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        activeModalClose(elem);
+    });
+})
+
+function activeModalClose(elem) {
+    let parentWrapper = elem.closest('.modal__wrapper');
+    let parentModal = elem.closest('.modal');
+    parentWrapper.classList.remove('modal__wrapper_active');
+    parentModal.classList.remove('modal_active');
+}
+
+function activeModal(elem) {
+    let wrapper = elem.childNodes[1];
+    wrapper.classList.add('modal__wrapper_active');
+    modal.classList.add('modal_active');
+}
+
+function activeModalRequest() {
+    const modalRequest = document.querySelector('.modal__request');
+    activeModal(modalRequest);
+}
+
+function activeModalOrder() {
+    const modalOrder = document.querySelector('.modal__order');
+    activeModal(modalOrder);
+}
+
+function activeModalFhanks() {
+    const modalFhanks = document.querySelector('.modal__fhanks');
+    activeModal(modalFhanks);
 }
